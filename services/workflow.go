436c6c25befd4bfddaf2b1b7266cc984390f7f36
keremeyuboglu <32223948+keremeyuboglu@users.noreply.github.com>
2024-07-21 23:40:36 +0300

    Moved Send Lead to Classifier to Project (#15)
    * Moved Send Lead to Classifier to Project
    
    * Sink URL needs to be changed.
    
    * Added error handling, SINK_URL as env for workflow

--- services/workflow.go
+++ services/workflow.go
@@ -1,10 +1,22 @@
 package services
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"log"
+	"log/slog"
+	"net/http"
+	"os"
 	"reflect"
 )
+
+var caseTypeMapping = map[string]int{
+	"Motor Vehicle Accident (MVA)": 4,
+	"Premises Liability":           15,
+	"Dog Bite":                     14,
+	"Other":                        2,
+}
 
 type Workflow struct {
 	TopicName    string
@@ -41,6 +53,90 @@ func (workflow Workflow) Call(params ...interface{}) (result interface{}, err er
 
 func ld_ld_sync(bytes []byte) error {
 	log.Println("Executing")
-	log.Println(string(bytes))
+
+	// Unmarshal the JSON byte array into the map
+	var jsonBody map[string]interface{}
+	err := json.Unmarshal(bytes, &jsonBody)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	if jsonBody["LeadStatus"] == "Referred Out" &&
+		jsonBody["LeadSubstatus"] == "Pending Review" &&
+		jsonBody["LeadReferredTo"] == "The Capital Law Firm" {
+		err := sendLeadToClf(jsonBody)
+		if err != nil {
+			slog.Error("Error occurred while sending lead to CLF", "error", err)
+			return err
+		}
+	}
+	return nil
+}
+
+// Should move to utils later
+
+func GetOrDefaultString(m map[string]interface{}, key string, defaultStr string) string {
+	if value, ok := m[key]; ok {
+		if str, ok := value.(string); ok {
+			return str
+		}
+	}
+	return defaultStr
+}
+
+func GetOrDefaultInt(m map[string]int, key string, defaultInt int) int {
+	if num, ok := m[key]; ok {
+		return num
+	}
+	return defaultInt
+}
+
+func sendLeadToClf(jsonBody map[string]interface{}) error {
+	defaultStr := ""
+	sinkUrl := os.Getenv("SINK_URL")
+	// sinkUrl := configuration.SINK_URL
+
+	leadCaseTypeStr := GetOrDefaultString(jsonBody, "LeadCaseType", "")
+
+	requestBody := map[string]interface{}{
+		"First":         GetOrDefaultString(jsonBody, "ContactFirstName", defaultStr),
+		"Last":          GetOrDefaultString(jsonBody, "ContactLastName", defaultStr),
+		"Phone":         GetOrDefaultString(jsonBody, "ContactMobilePhone", defaultStr),
+		"Email":         GetOrDefaultString(jsonBody, "ContactEmail", defaultStr),
+		"Summary":       GetOrDefaultString(jsonBody, "LeadSummary", defaultStr),
+		"Case_Type":     GetOrDefaultInt(caseTypeMapping, leadCaseTypeStr, 2),
+		"Incident_Date": GetOrDefaultString(jsonBody, "LeadIncidentDate", defaultStr),
+	}
+
+	jsonBodyBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	resp, err := http.Post(sinkUrl, "application/json", bytes.NewBuffer(jsonBodyBytes))
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		slog.Error("HTTP request failed",
+			"status_code", resp.StatusCode,
+			"status_text", http.StatusText(resp.StatusCode),
+		)
+		return errors.New("HTTP request failed with status code: " + http.StatusText(resp.StatusCode))
+	}
+
+	var responseBody map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&responseBody); err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	log.Printf("Status Code: %d", resp.StatusCode)
+	log.Printf("Response Body: %v", responseBody)
 	return nil
 }
